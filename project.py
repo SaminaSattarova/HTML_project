4958984177d242bc1153452eaac468cf4c98d1a3
Samina <Aydar.Sattarov@russianpost.ru>
2023-04-10 17:03:19 +0300

    подключение телеграмм-бота в отдельном файле (запускается отдельно от программы)

--- project.py
+++ project.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/project')
 def index():
     return render_template('base.html', title='9Н')
 
 
 if __name__ == '__main__':
-    app.run(port=7916, host='127.0.0.1')
+    app.run(port=7903, host='127.0.0.1')
